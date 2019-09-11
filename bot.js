69dfcbde203b098280f136bb363121697f3757ab
iKroos <55063785+iKroos@users.noreply.github.com>
2019-09-11 14:18:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -92,6 +92,33 @@ client.on('message', message => {
 });
 
 
+client.on('message', function(message) {
+    if(!message.channel.guild) return;
+    if(message.content === 'cc') {
+    if(message.member.hasPermission('MANAGE_ROLES')) {
+    setInterval(function(){})
+    message.channel.send('يتم انشاء 200 لون انتضر | ▶️')
+    }else{
+    message.channel.send('ما معاك البرمشن المطلوب |❌🚫')
+    }
+    }
+    });
+    
+    client.on('message', message=>{
+    if (message.content === 'cc'){
+    if(!message.channel.guild) return;
+    if (message.member.hasPermission('MANAGE_ROLES')){
+    setInterval(function(){})
+    let count = 0;
+    let ecount = 0;
+    for(let x = 1; x < 200; x++){
+    message.guild.createRole({name:x,
+    color: 'RANDOM'})
+    }
+    }
+    }
+    });
+
 
 
 client.login(process.env.BOT_TOKEN);
