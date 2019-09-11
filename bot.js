e2b0fbc4c5f75a9e1d36c1c6c7b66e95a09996b2
iKroos <55063785+iKroos@users.noreply.github.com>
2019-09-11 13:24:51 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-var prefix = "-";
-var adminprefix = '-'
-
+var prefix = "k";
+var adminprefix = 'k'
 
 const developers = ["527825056860995594","514876883951943733"]
 client.on('message', message => {
@@ -36,6 +35,30 @@ if (message.content.startsWith(adminprefix + 'setava')) {
   client.user.setAvatar(argresult);
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
+});
+
+
+
+
+
+
+
+
+
+
+client.on('messageCreate', async(message) => {
+  let prefix = 'k';
+  let args = message.cleanContent.split(' ');
+  if (args[0] == `${prefix}clear`) {
+    let botM = message.channel.guild.members.get(client.user.id);
+    if (!message.member.hasPermission('manageMessages') || !botM.hasPermission('manageMessages')) return;
+    let limit = !args[1] || isNaN(args[1]) || args[1] > 100 ? 100 : parseInt(args[1]);
+    let msgs = await message.channel.purge(limit != 100 ? limit + 1 : limit);
+    let msg = await message.channel.createMessage(`\`\`\`javascript\n${msgs != 100 ? msgs - 1 : msgs} message${msgs - 1 == 1 ? ' has' : 's have'} been deleted.\`\`\``);
+    setTimeout(() => {
+      msg.delete();
+    }, 2000);
+  }
 });
 
 
